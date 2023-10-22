f5fcef1df481f6b63aa0205b6dd57199dff4e761
BP <billpenn2005@outlook.com>
2023-10-22 17:37:46 +0800

    modified:   .gitignore 	modified:   Problem_data.xlsx 	modified:   src/greedy_Q.py

--- src/greedy_Q.py
+++ src/greedy_Q.py
@@ -14,7 +14,7 @@ def transfer_time(t):
 
 #get data
 problem_request=requests.get("https://oj.qd.sdu.edu.cn/api/problem/list?pageNow=1&pageSize=200")
-ac_request=requests.get("https://oj.qd.sdu.edu.cn/api/submit/list?pageNow=1&pageSize=20000") #https://oj.qd.sdu.edu.cn/api/submit/list?pageNow=1&pageSize=2000
+ac_request=requests.get("https://oj.qd.sdu.edu.cn/api/submit/list?pageNow=1&pageSize=200") #https://oj.qd.sdu.edu.cn/api/submit/list?pageNow=1&pageSize=2000
 problem_json=json.loads(problem_request.text)
 ac_json=json.loads(ac_request.text)
 
@@ -26,11 +26,11 @@ ac_dataframe=pd.DataFrame(ac_json['data']['rows'])
 problem_excel_dataframe=problem_dataframe[['problemId','problemCode','problemTitle','source','submitNum','acceptNum']]
 problem_excel_dataframe.rename(columns={"problemId":"题目id","problemCode":"题目代码","problemTitle":"标题","source":"题目来源","submitNum":"提交数","acceptNum":"通过数"},inplace=True)
 problem_excel_dataframe.to_excel('Problem_data.xlsx',index=False)
-ac_dataframe_excel=problem_dataframe[['submissionId','username','problemCode','problemTitle','judgeResult','judgeScore','judgeTemplateTitle','usedMemory','usedTime','gmtCreate']]
-for i in ac_dataframe_excel['gmtCreate']:
-    i=transfer_time(str(i))
-for i in ac_dataframe_excel['judgeResult']:
-    i=judge_state_dict(i)
+ac_dataframe_excel=ac_dataframe[['submissionId','username','problemCode','problemTitle','judgeResult','judgeScore','judgeTemplateTitle','usedMemory','usedTime','gmtCreate']]
+for index,row in ac_dataframe_excel.iterrows():
+    ac_dataframe_excel.at[index,'judgeResult']=judge_state_dict[row['judgeResult']]
+    ac_dataframe_excel.at[index,'gmtCreate']=transfer_time(str(row['gmtCreate']))
+ac_dataframe_excel.rename(columns={'submissionId':'ID','username':"用户名",'problemCode':'题目编号','problemTitle':'题目名','judgeResult':'测评结果','judgeScore':'测评得分','judgeTemplateTitle':'测评模板','usedMemory':'内存使用','usedTime':'时间使用(ms)','gmtCreate':'提交时间'},inplace=True)
 ac_dataframe_excel.to_excel('Judgement_data.xlsx',index=False)
 
 #sql
@@ -50,3 +50,16 @@ DB_URI = 'mysql+pymysql://{username}:{pwd}@{host}:{port}/{db}?charset=utf8'\
     .format(username =username,pwd = password,host = host,port=port,db = dbname)
 engine = create_engine(DB_URI, paramstyle="format")
 problem_excel_dataframe.to_sql('problem_data',engine,if_exists='replace',index=True)
+ac_dataframe_excel.to_sql('judgement_data',engine,if_exists='replace',index=True)
+
+#analyze
+problem_analyze_dataframe=pd.read_sql_table('problem_data',engine)
+problem_analyze_dataframe.drop('index',axis='columns',inplace=True)
+judge_analyze_dataframe=pd.read_sql_table('judgement_data',engine)
+judge_analyze_dataframe.drop('index',axis='columns',inplace=True)
+most_ac_dataframe=problem_analyze_dataframe.sort_values(by='通过数',axis=0,ascending=False)[:20]
+most_submit_dataframe=problem_analyze_dataframe.sort_values(by='提交数',axis=0,ascending=False)[:20]
+most_ac_dataframe.to_excel('Mostly_AC.xlsx',index=False)
+most_ac_dataframe.to_sql('most_ac_data',engine,if_exists='replace',index=False)
+most_submit_dataframe.to_excel('Mostly_Submitted.xlsx',index=False)
+most_submit_dataframe.to_sql('most_submitted_data',engine,if_exists='replace',index=False)
